938f2a27db00032632952124197c7406956c79ce
Rob Tice <rtice3@gmail.com>
2015-10-20 01:14:31 -0400

    Finished camera_list create camera sequence. Need to fix all the returns that return ints. Next working on camera class.

--- camera.cpp
+++ camera.cpp
@@ -1,87 +1,105 @@
 #include "camera.h"
 
-camera_list::camera_list() { gp_list_new(&d_list); }
-camera_list::~camera_list() { gp_list_free(d_list); }
-int camera_list::populate(GPContext* ctx) {
-        gp_list_reset(d_list);
-        return gp_camera_autodetect(d_list, ctx);
+orchid::camera_list::camera_list() { gp_list_new(&d_list); }
+
+orchid::camera_list::~camera_list() { gp_list_free(d_list); }
+
+int orchid::camera_list::refresh(GPContext* ctx) {
+    gp_list_reset(d_list);
+    return gp_camera_autodetect(d_list, ctx);
 }
-int camera_list::size() { gp_list_count(d_list); }
+
+int orchid::camera_list::size() { gp_list_count(d_list); }
+
 std::vector<std::string> get_name_list() {
-        std::vector<std::string> ret;
-        ret.reserve(this->size());
+    std::vector<std::string> ret;
+    ret.reserve(this->size());
 
-        for(int i = 0; i < this->size(); i++) {
-                const char* name;
-                gp_list_get_name(d_list, i, name);
-                ret.push_back(std::string(name));
-        }
+    for(int i = 0; i < this->size(); i++) {
+        const char* name;
+        gp_list_get_name(d_list, i, name);
+        ret.push_back(std::string(name));
+    }
+    return ret;
+}
+
+std::unique_ptr<orchid::camera> orchid::camera_list::create_camera(int index, GPContext* ctx) {
+    const char* model;
+    const char* port;
+    int ret, m, p;
+    CameraAbilities a;
+    GPPortInfo pi;
+    Camera* cam;
+
+    gp_list_get_name(d_list, index, &model);
+    gp_list_get_value(d_list, index, &port);
+
+    // TODO: fixx all return ints below. 
+
+    if((ret = gp_camera_new(&cam)) < GP_OK)
         return ret;
-}
-Camera* get_camera(int index) {
 
-}
-Camera* get_camera(std::string name) {
-        
-}
+    if(!d_abilities) {
+        if((ret = gp_abilities_list_new(&d_abilities)) < GP_OK)
+            return ret;
+        if((ret = gp_abilities_list_load(d_abilities, ctx)) < GP_OK)
+            return ret;
+    }
 
-camera::camera() {
-        d_ctx = gp_context_new();
-        gp_context_set_error_func(d_ctx, camera_context::ctx_error, NULL);
-        gp_context_set_status_func(d_ctx, camera_context::ctx_status, NULL);
+    if((m = gp_abilities_list_lookup_model(d_abilities, model)) < GP_OK)
+        return m;
+    if((ret = gp_abilities_list_get_abilities(d_abilities, m, &a)) < GP_OK)
+        return ret;
+    if((ret = gp_camera_set_abilities(cam, a)) < GP_OK)
+        return ret;
 
-        gp_list_new(&d_cam_list);
+    if(!d_pi_list) {
+        if((ret = gp_port_info_list_new(&d_pi_list)) < GP_OK)
+            return ret;
+        if((ret = gp_port_info_list_load(d_pi_list)) < GP_OK)
+            return ret;
+        if((gp_port_info_list_count(d_pi_list)) < GP_OK)
+            return ret;
+    }
 
+    if((p = gp_port_info_list_lookup_path(d_pi_list, port)) == GP_ERROR_UNKNOWN_PORT)
+        fprintf(stderr, "The port you specified ('%s') cannot be found.\n", port);
+    else if(p < GP_OK)
+        return p;
 
-        gp_camera_new (&d_cam);
-}
+    if((ret = gp_port_info_list_get_info(d_pi_list, p, &pi)) < GP_OK)
+        return ret;
+    if((ret = gp_camera_set_port_info(cam, pi)) < GP_OK)
+        return ret;
 
-camera::~camera() {
-        gp_list_free(d_cam_list);
-        gp_camera_exit(d_cam, d_ctx);
-        gp_camera_free(d_cam);
-        gp_context_unref(d_ctx);
-}
-
-int camera::init() {
-        /* This call will autodetect cameras, take the
-         * first one from the list and use it. It will ignore
-         * any others... See the *multi* examples on how to
-         * detect and use more than the first one.
-         */
-        ret = gp_camera_init (camera, context);
-        if (ret < GP_OK) {
-                printf("No camera auto detected.\n");
-                gp_camera_free (camera);
-                return 0;
-        }
-
-        /* Simple query the camera summary text */
-        ret = gp_camera_get_summary (camera, &text, context);
-        if (ret < GP_OK) {
-                printf("Camera failed retrieving summary.\n");
-                gp_camera_free (camera);
-                return 0;
-        }
-        printf("Summary:\n%s\n", text.text);
-
-        /* Simple query of a string configuration variable. */
-        ret = get_config_value_string (camera, "owner", &owner, context);
-        if (ret >= GP_OK) {
-                printf("Owner: %s\n", owner);
-                free (owner);
-        }
+    return std::move(std::unique_ptr<orchid::camera>(new orchid::camera(cam, ctx)));
 }
 
-
-
-static void camera::ctx_error(GPContext *context, const char *str, void *data) {
-        fprintf(stderr, "\n*** Contexterror ***              \n%s\n",str);
-        fflush(stderr);
+std::unique_ptr<orchid::camera> orchid::camera_list::create_camera(std::string name, GPContext* ctx) {
+    int ret, index;
+    if((ret = gp_list_find_by_name(d_list, index, name.c_str())) == GP_OK)
+        return this->get_camera(index, ctx);
+    return nullptr;
 }
 
-static void camera::ctx_status(GPContext *context, const char *str, void *data) {
-        fprintf(stderr, "%s\n", str);
-        fflush(stderr);
+orchid::camera::camera(Camera* cam, GPContext* ctx) : d_cam(cam), d_ctx(ctx) { }
+
+orchid::camera::~camera() {
+    gp_camera_exit(d_cam, d_ctx);
+    gp_camera_free(d_cam);
+}
+
+orchid::app::app() {
+    d_ctx = gp_context_new();
+
+    gp_context_set_error_func(d_ctx, camera_context::ctx_error, NULL);
+    gp_context_set_status_func(d_ctx, camera_context::ctx_status, NULL);
+}
+orchid::app::~app() {
+    gp_context_unref(d_ctx);
+}
+
+bool orchid::app::init() {
+    d_cam_list.refresh(d_ctx);
 }
 
--- camera.h
+++ camera.h
@@ -1,30 +1,48 @@
+#include <memory>
 #include <string>
+#include <vector>
 
 #include <gphoto2/gphoto2-camera.h>
 
-class camera_list {
-public:
-	camera_list();
-	~camera_list();
-	int populate();
-	int size();
-	std::vector<std::string> get_name_list();
-	Camera* get_camera(int);
-	Camera* get_camera(std::string);
-private:
-	CameraList* d_list;
-};
+namespace orchid {
 
-class camera {
-public:
-	camera();
-	~camera();
-	int init();
-private:
-	GPContext* d_ctx;
-	CameraList* d_cam_list;
-	Camera* d_cam;
+	class camera {
+	public:
+		camera(Camera*, GPContext*);
+		~camera();
+	private:
+		Camera* d_cam;
+		GPContext* d_ctx;
+	};
 
-	static void ctx_error(GPContext*, const char*, void*);
-	static void ctx_status(GPContext*, const char*, void*);
+	class camera_list {
+	public:
+		camera_list();
+		~camera_list();
+		int refresh(GPContext*);
+		int size();
+		std::vector<std::string> get_name_list();
+		std::unique_ptr<orchid::camera> create_camera(int, GPContext*);
+		std::unique_ptr<orchid::camera> create_camera(std::string, GPContext*);
+	private:
+		CameraList* d_list;
+		CameraAbilitiesList* d_abilities;
+		GPPortInfoList* d_pi_list;
+	};
+
+	using camera_ring = std::vector<std::unique_ptr<orchid::camera>>;
+
+	class app {
+	public:
+		app();
+		~app();
+		bool init();
+	private:
+		GPContext* d_ctx;
+		orchid::camera_list d_cam_list;
+		camera_ring d_cam_ring;
+
+		static void ctx_error(GPContext*, const char*, void*);
+		static void ctx_status(GPContext*, const char*, void*);
+	};
 };
