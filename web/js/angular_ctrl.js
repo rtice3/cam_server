f40317d390e414a88248ff6dc1b9ae53a1c9d8d1
Rob Tice <rtice3@gmail.com>
2015-11-03 22:44:14 -0500

    Adding web content. Prelim json parsing done in angular on client side.

---
+++ web/js/angular_ctrl.js
@@ -0,0 +1,41 @@
+(function() {
+  var app = angular.module('angular_app', ['ui.bootstrap', 'ngRoute']);
+
+  app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'html/camera-template.html',
+        controller: 'camera_ctrl',
+        controllerAs: 'ctrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+  app.factory('loading', [function() {  
+    var loader = {
+        request: function(config) {
+            $("#loading").show();
+            return config;
+        },
+        response: function(response) {
+            $("#loading").hide();
+            return response;
+        }
+    };
+    return loader;
+  }]);
+  app.config(['$httpProvider', function($httpProvider) {  
+    $httpProvider.interceptors.push('loading');
+  }]);
+
+  app.controller('camera_ctrl', [ '$http', function($http) {
+    var store = this;
+    store.data = [];
+    $http.get("json/test.json").success(function(response) {
+      store.data = response;
+      store.signature = true;
+    });
+  }]);
+})();
